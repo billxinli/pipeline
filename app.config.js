8683f803a70ee0d97da017b6b6cd47115997b93e
Bill Li <me@billxinli.com>
2021-03-23 18:20:59 -0400

    feat(pipeline): adding pipeline

--- app.config.js
+++ app.config.js
@@ -1,13 +1,13 @@
-import semverMajor from 'semver/functions/major'
-import semverMinor from 'semver/functions/minor'
-import semverPatch from 'semver/functions/patch'
+// import semverMajor from 'semver/functions/major'
+// import semverMinor from 'semver/functions/minor'
+// import semverPatch from 'semver/functions/patch'
 
 const VERSION = (process.env.VERSION || '0.0.0').replace('v', '')
 
-const major = semverMajor(VERSION)
-const minor = semverMinor(VERSION)
-const patch = semverPatch(VERSION)
-const versionCode = parseInt(major.toString().padStart(3, '0') + minor.toString().padStart(3, '0') + patch.toString().padStart(3, '0'), 10)
+// const major = semverMajor(VERSION)
+// const minor = semverMinor(VERSION)
+// const patch = semverPatch(VERSION)
+// const versionCode = parseInt(major.toString().padStart(3, '0') + minor.toString().padStart(3, '0') + patch.toString().padStart(3, '0'), 10)
 
 export default {
   'name': 'pipeline',
@@ -31,7 +31,7 @@ export default {
   },
   'android': {
     package: 'li.bill.pipeline',
-    versionCode,
+    // versionCode,
     'adaptiveIcon': {
       'foregroundImage': './assets/adaptive-icon.png',
       'backgroundColor': '#FFFFFF'
